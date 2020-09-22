97e88ea7e6dad827a5d706ba045b25032d1d03e0
Tuyet Le <toowee.le@gmail.com>
2020-09-22 13:02:59 +0100

    initialise state in the app component with an array of quotes fetched from api

--- src/App.js
+++ src/App.js
@@ -3,11 +3,41 @@ import Button from "./components/Button";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      quotes: [],
+      quoteIndex: null,
+    };
+    this.getQuoteIndex = this.getQuoteIndex.bind(this);
+  }
+
+  // Fetch quotes from an API
+  componentDidMount() {
+    const apiURL =
+      "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json";
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((quotes) =>
+        this.setState({ quotes }, () =>
+          this.setState({ quoteIndex: this.getQuoteIndex() })
+        )
+      );
+  }
+
+  getQuoteIndex() {
+    const { quotes } = this.state;
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    if (!quotes.length) return;
+    return randomIndex;
+  }
+
   handleNextQuoteClick() {
     console.log("clicked");
   }
 
   render() {
+    console.log(this.state.quoteIndex);
     return (
       <div className="App" id="quote-box">
         <Button handleClick={this.handleNextQuoteClick} />
