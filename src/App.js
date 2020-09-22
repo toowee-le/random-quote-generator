b7974a72b7dce3e7782db545140b447eb7f7eee7
Tuyet Le <toowee.le@gmail.com>
2020-09-22 15:38:05 +0100

    add/update comments on app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Button from "./components/Button";
 import "./App.css";
-import { render } from "react-dom";
 
 class App extends Component {
   constructor(props) {
@@ -12,7 +11,9 @@ class App extends Component {
     };
   }
 
-  // Fetch quotes from an API
+  /**
+   * @description Fetch quotes from API
+   */
   componentDidMount() {
     const apiURL =
       "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json";
@@ -25,17 +26,22 @@ class App extends Component {
       );
   }
 
+  /**
+   * @description Returns a random integer between 0 and the length of the array to represent an index in this.state.quotes.
+   * If array is empty, return undefined.
+   */
   generateRandomQuoteIndex = () => {
     const { quotes } = this.state;
     const randomIndex = Math.floor(Math.random() * quotes.length);
-    // Check if array is empty otherwise return the random index
     if (!quotes.length) return;
     return randomIndex;
   };
 
-  // Get the randomly chosen quote from array
+  /**
+   * @description Retrieve the selected quote from array (this.state.quotes).
+   * If array is empty or index is not a number, return undefined.
+   */
   get selectedQuote() {
-    // Return undefined if array is empty or index is not a number
     if (!this.state.quotes.length || !Number.isInteger(this.state.quoteIndex))
       return undefined;
     return this.state.quotes[this.state.quoteIndex];
