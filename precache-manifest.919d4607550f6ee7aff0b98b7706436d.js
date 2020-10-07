4a93b0f6962c92a6aa71e57e369a19687d9d56c2
Tuyet Le <toowee.le@gmail.com>
2020-10-07 15:05:19 +0100

    Updates

---
+++ precache-manifest.919d4607550f6ee7aff0b98b7706436d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4883d08dc0ebba9fee472a36094e996a",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "2f733dc77fcda940e9ee",
+    "url": "/random-quote-generator/static/css/main.65d2a7b5.chunk.css"
+  },
+  {
+    "revision": "be4373e6b46805a15ea2",
+    "url": "/random-quote-generator/static/js/2.62db6165.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/random-quote-generator/static/js/2.62db6165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f733dc77fcda940e9ee",
+    "url": "/random-quote-generator/static/js/main.62b96639.chunk.js"
+  },
+  {
+    "revision": "683fb47f88b32bd6eb13",
+    "url": "/random-quote-generator/static/js/runtime-main.c79c7f75.js"
+  }
+]);
